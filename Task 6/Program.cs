77a9f8079333f770f2cab2168368e476bb216e07
Adit Tamang <tamangadit86@gmail.com>
2025-11-05 10:15:14 +0545

    Task 6: Generic Collections (List and Dictionary

---
+++ Task 6/Program.cs
@@ -0,0 +1,33 @@
+﻿// List of favorite fruits
+List<string> fruits = new List<string> { "Apple", "Banana", "Mango" };
+
+// Add a new fruit
+fruits.Add("Orange");
+
+// Remove a fruit
+fruits.Remove("Banana");
+
+// Print all fruits
+Console.WriteLine("Fruits in the list:");
+foreach (var fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+// Dictionary of fruit IDs
+Dictionary<int, string> fruitDict = new Dictionary<int, string>
+{
+    { 1, "Apple" },
+    { 2, "Banana" },
+    { 3, "Mango" }
+};
+
+// Add a new entry
+fruitDict.Add(4, "Orange");
+
+// Print all dictionary entries
+Console.WriteLine("\nFruit Dictionary:");
+foreach (var item in fruitDict)
+{
+    Console.WriteLine("ID: " + item.Key + ", Fruit: " + item.Value);
+}
