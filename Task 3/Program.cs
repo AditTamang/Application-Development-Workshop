454bc61c557a1bcf836aa4eac4b80c12b2044ad2
Adit Tamang <tamangadit86@gmail.com>
2025-11-05 10:05:05 +0545

    Task 3: Data Types and Type Conversion

---
+++ Task 3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Variable declarations
+byte myByte = 255;
+short myShort = 94;
+int myInt = 42;
+long myLong = 15;
+float myFloat = 3.12f;
+double myDouble = 3.14159343;
+decimal myDecimal = 3.1415926535897932384626433832m;
+char myChar = 'M';
+bool isGarib = true;
+
+// Conversions
+int number = 42;
+string numberAsString = number.ToString();   // int → string
+
+string strValue = "3.14";
+double strToDouble = Convert.ToDouble(strValue); // string → double
+
+// Print results
+Console.WriteLine("byte: " + myByte);
+Console.WriteLine("short: " + myShort);
+Console.WriteLine("int: " + myInt);
+Console.WriteLine("long: " + myLong);
+Console.WriteLine("float: " + myFloat);
+Console.WriteLine("double: " + myDouble);
+Console.WriteLine("decimal: " + myDecimal);
+Console.WriteLine("char: " + myChar);
+Console.WriteLine("bool: " + isGarib);
+Console.WriteLine("int to string: " + numberAsString);
+Console.WriteLine("string to double: " + strToDouble);
