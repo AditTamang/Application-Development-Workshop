db5d7ace133f40252ffa81e4103f377d791a85a1
Adit Tamang <tamangadit86@gmail.com>
2025-11-05 10:12:49 +0545

    Task 5: Array and Array Methods

---
+++ Task 5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Birthdate
+DateTime birthDate = new DateTime(2004, 11, 21); // <-- change to your birthday
+
+// Current date and time
+DateTime currentDate = DateTime.Now;
+
+// Calculate age using TimeSpan
+TimeSpan ageSpan = currentDate - birthDate;
+double ageInYears = ageSpan.Days / 365.25; // approximate years
+
+// Print results
+Console.WriteLine("Birthdate: " + birthDate.ToShortDateString());
+Console.WriteLine("Current Date: " + currentDate.ToShortDateString());
+Console.WriteLine("Your Age (Years): " + (int)ageInYears);
+
+// Add 10 days to your birthdate
+DateTime newDate = birthDate.AddDays(10);
+Console.WriteLine("Birthdate + 10 days: " + newDate.ToShortDateString());
