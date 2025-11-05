24be91d4081aecb4a4b7c436c6e967e7817e3828
Adit Tamang <tamangadit86@gmail.com>
2025-11-05 09:50:09 +0545

    Task 2: Class

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Circle
+{
+    const double PI = 3.14;
+    public static void Main()
+    {
+        double radius = 5.0;
+        double PI = 3.14159; 
+        double area = PI * radius * radius;
+        Console.WriteLine($"The area of the circle is {area}");
+    }
+}
